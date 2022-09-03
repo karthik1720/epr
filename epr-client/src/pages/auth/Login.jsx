698e1e7d3a09ffaad1e51f8d02bf0e9d767d9961
karthik <karthik.cs1720@gmail.com>
2022-09-04 00:28:12 +0530

    in development stage

--- epr-client/src/pages/auth/Login.jsx
+++ epr-client/src/pages/auth/Login.jsx
@@ -1,23 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import "./login.css";
+import axios from "axios";
 
 function Login() {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    setUser((prev) => {
+      if (event.target.name === "username")
+        return {
+          username: event.target.value,
+          email: prev.email,
+          password: prev.password,
+        };
+      if (event.target.name === "email")
+        return {
+          email: event.target.value,
+          username: prev.username,
+          password: prev.password,
+        };
+      if (event.target.name === "password")
+        return {
+          password: event.target.value,
+          email: prev.email,
+          username: prev.username,
+        };
+    });
+  };
+
+  const handleSubmit = async () => {
+    const loginPayload = {
+      name: user.username,
+      password: user.password,
+    };
+
+    await axios
+      .post("http://localhost:8080/api/login", loginPayload)
+      .then((response) => {
+        //get token from response
+        const token = response.data;
+
+        //set JWT token to local
+        localStorage.setItem("token", token);
+
+        //set token to axios common header
+        console.log(token);
+
+        //redirect user to home page
+        window.location.href = "/";
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="loginContainer">
       <div className="loginItems">
-        <label htmlFor="" className="loginLabel">
-          Username
-        </label>
-        <label htmlFor="" className="loginLabel">
-          Email
-        </label>
-        <label htmlFor="" className="loginLabel">
-          Password
-        </label>
+        <input
+          name="username"
+          type="text"
+          value={user.username}
+          onChange={handleChange}
+          placeholder="Username"
+          className="loginInput"
+        />
       </div>
       <div className="loginItems">
-        <input type="text" className="loginInput" />
-        <input type="email" className="loginInput" />
-        <input type="password" className="loginInput" />
+        <input
+          name="email"
+          type="email"
+          value={user.email}
+          onChange={handleChange}
+          placeholder="email"
+          className="loginInput"
+        />
+      </div>
+      <div className="loginItems">
+        <input
+          name="password"
+          type="password"
+          value={user.password}
+          onChange={handleChange}
+          placeholder="password"
+          className="loginInput"
+        />
+      </div>
+      <div className="loginItems">
+        <button onClick={handleSubmit}>Login</button>
       </div>
     </div>
   );
